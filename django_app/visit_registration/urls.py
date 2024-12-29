86d77008e030975ef9afa88644b9be491d455e26
Daniel Kalmucki <danielkalmucki2@gmail.com>
2024-12-29 15:22:38 +0100

    Add authentication and views based on the account type

--- django_app/visit_registration/urls.py
+++ django_app/visit_registration/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('register/', views.register_appointment, name='register'),
     path('success/', views.success, name='success'),
+    path('login/', views.CustomLoginView.as_view(template_name='login.html'), name='login'),
+    path('patient/dashboard', views.patient_dashboard, name='patient_dashboard'),
+    path('doctor/dashboard', views.doctor_dashboard, name='doctor_dashboard'),
 ]
--- django_app/visit_registration/views.py
+++ django_app/visit_registration/views.py
@@ -1,5 +1,37 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, resolve_url
+from django.urls import reverse_lazy, reverse
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView
 from .forms import AppointmentForm
+from django.contrib.auth.models import Group, Permission
+
+
+if not Group.objects.get(name='patient'):
+    Group.objects.create(name='patient')
+if not Group.objects.get(name='doctor'):
+    Group.objects.create(name='doctor')
+
+
+def is_doctor(user):
+    return user.groups.filter(name='doctor').exists()
+
+
+def is_patient(user):
+    return user.groups.filter(name='patient').exists()
+
+
+@login_required
+@user_passes_test(is_doctor)
+def doctor_view(request):
+    return render(request, 'doctor_dashboard.html')
+
+
+@login_required
+@user_passes_test(is_doctor)
+def patient_view(request):
+    return render(request, 'patient_dashboard.html')
 
 
 def register_appointment(request):
@@ -15,3 +47,25 @@ def register_appointment(request):
 
 def success(request):
     return render(request, 'success.html')
+
+
+@login_required
+@user_passes_test(is_patient)
+def patient_dashboard(request):
+    return render(request, 'patient_dashboard.html')
+
+
+@login_required
+@user_passes_test(is_doctor)
+def doctor_dashboard(request):
+    return render(request, 'doctor_dashboard.html')
+
+
+class CustomLoginView(LoginView):
+    def get_success_url(self):
+        if is_doctor(self.request.user):
+            return resolve_url('doctor_dashboard')
+        if is_patient(self.request.user):
+            return resolve_url('patient_dashboard')
+        else:
+            return resolve_url('doctor_dashboard')
